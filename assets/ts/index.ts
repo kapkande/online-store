4be216b9ceaac4ca69436006329dee5480f1d521
kapkande <kap.kande@gmail.com>
2022-12-19 21:30:28 +0300

    init: add functiom showblocks

--- assets/ts/index.ts
+++ assets/ts/index.ts
@@ -1,30 +1,30 @@
 type elemAndNull = Element | null;
-const mainProduct: elemAndNull  = document.querySelector('.main__product');
-type stringAndUndefined = string| undefined
-type stringNull =  string | null
+const mainProduct: elemAndNull = document.querySelector('.main__product');
+type stringAndUndefined = string | undefined
+type stringNull = string | null
 
-function setBlock(scr:stringAndUndefined, name:stringAndUndefined, description:stringAndUndefined, cost:stringAndUndefined):void {
+function setBlock(obj): void {
     let block = document.createElement("div"); //? какой тут тип
     block.classList.add('product__block');
-    
+
     //create image
     let img = document.createElement("div");
     img.classList.add('product__block__image');
-    img.style.backgroundImage = `url('${scr}')`;
+    img.style.backgroundImage = `url('${obj.imageMain}')`;
     //create name
-    let nameItem:Element = document.createElement("div");
+    let nameItem: Element = document.createElement("div");
     nameItem.classList.add('product__block__name-item');
-    nameItem.textContent = `${name}`;
+    nameItem.textContent = `${obj.name}`;
     //create description
-    let descriptionItem:Element = document.createElement("div");
+    let descriptionItem: Element = document.createElement("div");
     descriptionItem.classList.add('product__block__description');
-    descriptionItem.textContent = `${description}`;
+    descriptionItem.textContent = `${obj.description}`;
     //create cost
-    let costItem:Element = document.createElement("div");
+    let costItem: Element = document.createElement("div");
     costItem.classList.add('product__block__description');
-    costItem.textContent = `${cost}`
+    costItem.textContent = `${obj.cost}`
     //create btm 
-    let btm:Element = document.createElement("button");
+    let btm: Element = document.createElement("button");
     btm.classList.add('product__block__button');
     btm.textContent = 'add to'
 
@@ -35,4 +35,24 @@ function setBlock(scr:stringAndUndefined, name:stringAndUndefined, description:s
     block.appendChild(btm);
     mainProduct?.appendChild(block);
 }
-setBlock(basic[0][0].imageMain, basic[0][0].name, basic[0][0].description, basic[0][0].cost)
+
+function getOrder() {
+    const newArray:Array<object> = [];
+    for (let i = 0; i < basic.length;) {
+        let random = Math.floor(Math.random() * (basic.length - 1 + 1)) + 0;
+        if (!newArray.includes(basic[random])){
+            newArray.push(basic[random]);
+            i++
+        }
+    }
+return newArray   
+}
+
+function showBlocks() {
+   let arrey:Array<object> = getOrder();
+   arrey.forEach(element => {
+    setBlock(element)
+   });
+
+}
+showBlocks()
